1e2edc5c1d227f8bf9ce89cd8d489e138aca0929
Laniran Peter <laniranpeter@gmail.com>
2024-10-18 15:52:51 +0100

    format

--- src/service/bucket/types.ts
+++ src/service/bucket/types.ts
@@ -1,55 +1,53 @@
-
 export interface ListParams {
-    page: number;
-    limit: number;
-    startDate: Date;
-    endDate: Date;
+  page: number
+  limit: number
+  startDate: Date
+  endDate: Date
 }
-  
 export interface Bucket {
-    createdBy: {
-      userId: string;
-      fullName: string;
-    };
-    bucketName: string;
-    location: string;
-    createdAt: string;
-    bucketId: string;
-    region: string;
+  createdBy: {
+    userId: string
+    fullName: string
+  }
+  bucketName: string
+  location: string
+  createdAt: string
+  bucketId: string
+  region: string
 }
-  
+
 export interface IObject {
-    createdBy: {
-      userId: string;
-      fullName: string;
-    };
-    objectKey: string;
-    location: string;
-    createdAt: string;
-    fileFormat: string;
-    objectId: string;
-    bucket: string
-    fileSize: string
+  createdBy: {
+    userId: string
+    fullName: string
+  }
+  objectKey: string
+  location: string
+  createdAt: string
+  fileFormat: string
+  objectId: string
+  bucket: string
+  fileSize: string
 }
 
 export interface CreateBucket {
-    bucketName: string;
-    region: string;
-    isPrivate?: boolean;
+  bucketName: string
+  region: string
+  isPrivate?: boolean
 }
 
 export interface Pagination {
-    totalDocs: number;
-    limit: number;
-    page: number;
-    totalPages: number;
-    pagingCounter: number;
-    prevPage: number | null;
-    nextPage: number | null;
-  }
-  
+  totalDocs: number
+  limit: number
+  page: number
+  totalPages: number
+  pagingCounter: number
+  prevPage: number | null
+  nextPage: number | null
+}
+
 export interface ApiResponse<T> {
-   success: boolean
-   message: string
-   data?: T
+  success: boolean
+  message: string
+  data?: T
 }
